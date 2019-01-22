71bf6feaf6d873d38b02f8bff557650645022f00
sbx_user1059 <sbx_user1059@ip-10-129-216-1.eu-west-2.compute.internal>
2019-01-22 23:11:47 +0000

    Tue Jan 22 23:11:47 UTC 2019

--- src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -6,7 +6,27 @@ namespace BeFaster.App.Solutions.FIZ
     {
         public static string FizzBuzz(int number)
         {
-            throw new SolutionNotImplementedException();
+
+
+            int ofThree = number ^ 3;
+            var ofFive = number ^ 5;
+
+            if (ofThree == 0 && ofFive == 0) {
+                return "fizz buzz";
+            }else if (ofFive== 0 && ofThree != 0) {
+                return "buzz";
+            }else if (ofFive !=0  && ofThree == 0) {
+                return "fizz";
+            }else
+            {
+                return "example 1";
+            }
+
+
+
+
+
         }
     }
 }
+
