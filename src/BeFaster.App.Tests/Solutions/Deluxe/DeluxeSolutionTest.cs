6dfe43fb7397f03ad8d8d635814d7c5ba88f5d86
sbx_user1059 <sbx_user1059@ip-10-129-216-1.eu-west-2.compute.internal>
2019-01-22 23:41:55 +0000

    Tue Jan 22 23:41:55 UTC 2019

---
+++ src/BeFaster.App.Tests/Solutions/Deluxe/DeluxeSolutionTest.cs
@@ -0,0 +1,13 @@
+﻿using BeFaster.App.Solutions.SUM;
+using NUnit.Framework;
+
+
+namespace Deluxe
+{
+    public class DeluxeSolutionTest
+    {
+        public DeluxeSolutionTest()
+        {
+        }
+    }
+}
